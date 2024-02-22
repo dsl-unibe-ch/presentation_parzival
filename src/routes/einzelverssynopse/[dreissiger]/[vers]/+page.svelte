5adef8aed8b707fa368f6bd8b0b8a473851541f1
Sebastian <sebastian@flicks.jetzt>
2024-02-22 15:15:58 +0100

    update sigla and requests

--- src/routes/einzelverssynopse/[dreissiger]/[vers]/+page.svelte
+++ src/routes/einzelverssynopse/[dreissiger]/[vers]/+page.svelte
@@ -1,19 +1,23 @@
 <script>
 	import { onMount } from 'svelte';
-	import sigla from '$lib/sigla';
+	import sigla from '$lib/sigla.json';
 
 	/** @type {import('./$types').PageData} */
 	export let data;
 
-	let publisherData;
+	let publisherData = {};
 
 	onMount(() => {
-		sigla.actual.forEach((element) => {
+		const archetype = sigla.hyparchetypes.map((e) => e.handle);
+		const witnesses = sigla.codices.map((e) => e.handle);
+		console.log(archetype);
+		console.log(witnesses);
+		witnesses.forEach((element) => {
 			publisherData[element] = fetch(
-				`https://tei-ub.dh.unibe.ch/exist/apps/parzival/api/parts/${element.toLowerCase()}.xml/json?odd=parzival.odd&view=single&xpath=//text/body/l[@xml:id=%27${element.toLowerCase()}_${data.dreissiger}.${data.vers}%27]`
+				`https://tei-ub.dh.unibe.ch/exist/apps/parzival/api/parts/${element}.xml/json?odd=parzival.odd&view=single&xpath=//text/body/l[@xml:id=%27${element}_${data.dreissiger}.${data.vers}%27]`
 			).then((r) => r.json());
 		});
-		sigla.normalized.forEach((element) => {
+		archetype.forEach((element) => {
 			publisherData[element] = fetch(
 				`https://tei-ub.dh.unibe.ch/exist/apps/parzival/api/parts/syn${data.dreissiger}.xml/json?odd=parzival.odd&view=single&xpath=//text/body/div/div/l[@n=%27${element}%20${data.dreissiger}.${data.vers}%27]`
 			).then((r) => r.json());
