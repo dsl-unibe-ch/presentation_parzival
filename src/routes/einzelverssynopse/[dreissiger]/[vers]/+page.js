98f07cbf9ab6514383f05eca6fc3e041a90221c2
Sebastian Flick <sebastian.flick@wbkolleg.unibe.ch>
2024-03-06 13:34:26 +0100

    add data-update and workflow

--- src/routes/einzelverssynopse/[dreissiger]/[vers]/+page.js
+++ src/routes/einzelverssynopse/[dreissiger]/[vers]/+page.js
@@ -5,3 +5,20 @@ export async function load({ params }) {
 		vers: params.vers.padStart(2, '0')
 	};
 }
+
+/** @type {import('./$types').EntryGenerator} */
+export function entries() {
+	// Generate all 827 Dreissiger with 1-30 verses
+	/** @type {import('./$types').RouteParams[]} */
+	const entryArray = [];
+	for (let dreissiger = 1; dreissiger <= 827; dreissiger++) {
+		for (let vers = 1; vers <= 30; vers++) {
+			entryArray.push({
+				dreissiger: `${dreissiger}`,
+				vers: `${vers}`
+			});
+		}
+	}
+
+	return entryArray;
+}
