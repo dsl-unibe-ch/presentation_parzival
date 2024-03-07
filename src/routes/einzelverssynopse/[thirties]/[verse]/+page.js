0c0098ade0ac4afe5e925640b762ca5ff91874b1
Sebastian <sebastian@flicks.jetzt>
2024-03-07 10:30:06 +0100

    ditch setting limits

--- src/routes/einzelverssynopse/[thirties]/[verse]/+page.js
+++ src/routes/einzelverssynopse/[thirties]/[verse]/+page.js
@@ -11,7 +11,16 @@ export function entries() {
 	// Generate all 827 Dreissiger with 1-30 verses
 	/** @type {import('./$types').RouteParams[]} */
 	const entryArray = [];
-	for (let thirties = 1; thirties <= 827; thirties++) {
+	/*for (let thirties = 1; thirties <= 827; thirties++) {
+		for (let verse = 1; verse <= 30; verse++) {
+			entryArray.push({
+				thirties: `${thirties}`,
+				verse: `${verse}`
+			});
+		}
+	}*/
+	// too many open files :(
+	for (let thirties = 1; thirties <= 2; thirties++) {
 		for (let verse = 1; verse <= 30; verse++) {
 			entryArray.push({
 				thirties: `${thirties}`,
