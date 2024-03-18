ad94779f942b477271b00549fff419dfca2e798f
Sebastian Flick <sebastian.flick@wbkolleg.unibe.ch>
2024-03-18 22:07:34 +0100

    add d3

---
+++ src/routes/Devilstable.svelte
@@ -0,0 +1,14 @@
+<script>
+	let data = [
+		{
+			label: 'D',
+			value: 1
+		},
+		{ label: 'D', value: 12 },
+		{ label: 'D', value: undefined },
+		{ label: 'D', value: 15 },
+		{ label: 'D', value: 30 },
+		{ label: 'n', value: 1 },
+		{ label: 'n', value: 30 }
+	];
+</script>
