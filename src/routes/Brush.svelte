80f61a83060a7622b693a70daaabe07dd7a711b9
Sebastian Flick <sebastian.flick@wbkolleg.unibe.ch>
2024-04-10 10:41:18 +0200

    finalize brush width-view

--- src/routes/Brush.svelte
+++ src/routes/Brush.svelte
@@ -9,7 +9,7 @@
 	let marginTop = 20;
 	let marginRight = 0;
 	let marginBottom = 20;
-	let marginLeft = 28;
+	$: marginLeft = mobile ? 30 : 35;
 	const optimalChunkWidth = 18;
 	export let DATA_MIN = 1;
 	export let DATA_MAX = 827;
@@ -41,8 +41,11 @@
 			values: []
 		}
 	];
+	$: mobile = width > height;
 	$: availableWidth = width - marginLeft - marginRight;
-	$: numChunks = Math.max(Math.floor(availableWidth / optimalChunkWidth), 1);
+	$: numChunks = mobile
+		? Math.max(Math.floor(availableWidth / optimalChunkWidth), 1)
+		: Math.max(Math.floor((height - marginTop - marginBottom) / optimalChunkWidth), 1);
 	$: colorScale = d3
 		.scaleThreshold()
 		.domain([0.001, 1 / 4, 2 / 4, 3 / 4, 0.9999])
@@ -50,6 +53,24 @@
 	// $: colorScale = d3.scaleQuantize([0, 1], ['50', '200', '400', '500', '600', '900']);
 
 	// create chunks: each chunk is a number counting the number of true values in the chunk
+	$: sourcesDim = d3.scaleBand().domain(data.map((d) => d.label));
+	$: xChunk = d3
+		.scaleLinear()
+		.domain([0, numChunks])
+		.range(mobile ? [marginLeft, width - marginRight] : [marginBottom, height - marginTop]);
+
+	$: valuesDim = d3.scaleLinear().domain([DATA_MIN, DATA_MAX]);
+	/** @type any */
+	let x, /** @type any */ y;
+	$: {
+		if (mobile) {
+			x = valuesDim.range([marginLeft, width - marginRight]);
+			y = sourcesDim.range([height - marginTop, marginBottom]);
+		} else {
+			x = sourcesDim.range([marginLeft, width - marginRight]);
+			y = valuesDim.range([height - marginTop, marginBottom]);
+		}
+	}
 	$: chunkedData = data.map((dataObject) => {
 		const chunked = new Array(numChunks).fill(0);
 		const chunkedPresent = new Array(numChunks).fill(0);
@@ -57,7 +78,7 @@
 			const start = xChunk(i);
 			const end = xChunk(i + 1);
 			dataObject.values.forEach((present, valIndex) => {
-				const pos = x(valIndex);
+				const pos = valuesDim(valIndex);
 				if (pos >= start && pos < end) {
 					chunked[i]++;
 					if (present) {
@@ -76,46 +97,46 @@
 			})
 		};
 	});
-
-	$: y = d3.scaleBand(
-		data.map((d) => d.label),
-		[height - marginTop, marginBottom]
-	);
-	$: xChunk = d3.scaleLinear([0, numChunks], [marginLeft, width - marginRight]);
-	$: x = d3.scaleLinear([DATA_MIN, DATA_MAX], [marginLeft, width - marginRight]);
 	$: d3.select(gy).call(d3.axisLeft(y));
 	$: d3.select(gx).call(d3.axisBottom(x));
+	/**
+	 * @type {d3.brushX | d3.brushY}
+	 */
+	let brush;
+	$: {
+		brush = mobile ? d3.brushX() : d3.brushY();
+		brush
+			.extent([
+				[marginLeft, 0],
+				[width - marginRight, height - marginBottom - marginTop]
+			])
+			.on('brush', (/** @type {{ selection: [number, number]; }} */ e) => {
+				const from = mobile ? e.selection[0] : e.selection[1];
+				const to = mobile ? e.selection[1] : e.selection[0];
+				if (to - from <= 180) {
+					const start = Math.round(valuesDim.invert(from));
+					const end = Math.round(valuesDim.invert(to));
 
-	$: brush = d3
-		.brushX()
-		.extent([
-			[marginLeft, 0],
-			[width - marginRight, height - marginBottom - marginTop]
-		])
-		.on('brush', (/** @type {{ selection: [number, number]; }} */ e) => {
-			const selection = e.selection;
-			if (selection[1] - selection[0] <= 180) {
-				const start = Math.round(x.invert(selection[0]));
-				const end = Math.round(x.invert(selection[1]));
+					dispatch('brush', { start, end });
+				}
+			})
+			.on('end', (/** @type {{ selection: [number, number]; }} */ e) => {
+				const from = mobile ? e.selection[0] : e.selection[1];
+				const to = mobile ? e.selection[1] : e.selection[0];
+				if (to - from > 180) {
+					const start = Math.round(valuesDim.invert(from));
+					const end = Math.round(valuesDim.invert(to));
 
-				dispatch('brush', { start, end });
-			}
-		})
-		.on('end', (/** @type {{ selection: [number, number]; }} */ e) => {
-			const selection = e.selection;
-			if (selection[1] - selection[0] > 180) {
-				const start = Math.round(x.invert(selection[0]));
-				const end = Math.round(x.invert(selection[1]));
-
-				dispatch('brush', { start, end });
-			}
-		});
+					dispatch('brush', { start, end });
+				}
+			});
+	}
 	$: d3.select(gBrush)
 		.call(brush)
-		.call(brush.move, [x(DATA_MIN), x(100)]);
+		.call(brush.move, [valuesDim(DATA_MIN), valuesDim(100)]);
 </script>
 
-<svg {width} {height}>
+<svg {width} {height} class="float-left">
 	<g bind:this={gy} transform="translate({marginLeft - 5} ,0)" />
 	<g bind:this={gx} transform="translate(0,{height - marginBottom})" />
 	{#each chunkedData as d}
@@ -124,10 +145,10 @@
 				{@const start = xChunk(j)}
 				{@const end = xChunk(j + 1)}
 				<rect
-					x={start}
-					y={y(d.label)}
-					width={end - start}
-					height={y.bandwidth()}
+					x={mobile ? start : x(d.label)}
+					y={mobile ? y(d.label) : start}
+					width={mobile ? end - start : x.bandwidth()}
+					height={mobile ? y.bandwidth() : end - start}
 					fill={`rgb(var(--color-primary-${colorScale(v)}))`}
 				/>
 			{/each}
