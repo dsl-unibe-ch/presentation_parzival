f0da7bd807eefc0fadd52010afc0ad8c8895df50
Sebastian Flick <sebastian.flick@wbkolleg.unibe.ch>
2024-06-03 17:45:34 +0200

    call api for static metadata

--- src/routes/api/json/metadata-ms-page/[sigla]/[thirties]/[verse]/+server.js
+++ src/routes/api/json/metadata-ms-page/[sigla]/[thirties]/[verse]/+server.js
@@ -2,10 +2,13 @@ import { generateEntries } from '$lib/functions';
 import { error } from '@sveltejs/kit';
 
 /** @type {import('./$types').RequestHandler} */
-export async function GET({ params }) {
+export async function GET({ params, fetch }) {
 	let meta;
 	try {
-		const data = await import(`../../../../../../../lib/metadata-ms-page/${params.sigla}.json`);
+		const data = await fetch(
+			`https://dhbern.github.io/parzival-static-api/api/json/metadata-ms-page/${params.sigla}.json`
+		).then((r) => r.json());
+		console.log(data);
 		meta = data[params.sigla].find((/** @type {{ l: string, id:string | string[]; }} */ entry) =>
 			entry.l.includes(`${params.thirties}.${params.verse.padStart(2, '0')}`)
 		);
@@ -14,4 +17,9 @@ export async function GET({ params }) {
 		console.error(e);
 		return error(404, 'Not found');
 	}
+}
+
+/** @type {import('./$types').EntryGenerator} */
+export function entries() {
+	return generateEntries(true);
 }
