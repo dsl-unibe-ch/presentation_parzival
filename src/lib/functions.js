6bc6d4f4c2f3ca6cc0eacb0d384a5f92821cb622
Sebi <sebastian@flick.solutions>
2024-06-11 13:54:53 +0200

    slim down entry-generation
    don't prerender for every verse

--- src/lib/functions.js
+++ src/lib/functions.js
@@ -3,7 +3,7 @@ import { codices } from '$lib/sigla.json';
 /**
  * Generate all 827 Dreissiger with 1-30 verses for all sigla
  * @param {boolean} sigla
- * @returns {Array<{ sigla?: string, thirties: string, verse: string }>}
+ * @returns {Array<{ sigla?: string, thirties: string, verse?: string }>}
  */
 export function generateEntries(sigla) {
 	const entryArray = [];
@@ -11,13 +11,10 @@ export function generateEntries(sigla) {
 		const siglaArray = codices.map((codex) => codex.handle);
 		for (/** @type string */ let sigla of siglaArray) {
 			for (let thirties = 1; thirties <= 827; thirties++) {
-				for (let verse = 1; verse <= 30; verse++) {
-					entryArray.push({
-						sigla: sigla,
-						thirties: `${thirties}`,
-						verse: `${verse}`
-					});
-				}
+				entryArray.push({
+					sigla: sigla,
+					thirties: `${thirties}`
+				});
 			}
 		}
 	} else {
