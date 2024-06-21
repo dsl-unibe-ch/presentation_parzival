4472ccba0a163733f45b2dcdf4b760f216cf7037
Sebastian Flick <sebastian.flick@wbkolleg.unibe.ch>
2024-06-21 09:31:46 +0200

    create IIIF-Component to keep code clean

---
+++ src/lib/components/IIIFViewer.svelte
@@ -0,0 +1,124 @@
+<script context="module">
+	import { assets } from '$app/paths';
+	/**
+	 * @type {import('openseadragon')}
+	 */
+	let OpenSeadragon;
+</script>
+
+<script>
+	/** @type { Promise<Object> } */
+	export let iiif;
+
+	const uuid = crypto.randomUUID();
+	/**
+	 * @type {import('openseadragon').Viewer}
+	 */
+	let viewer;
+
+	const generateViewer = (/** @type {Element} */ node, /** @type {Promise<Object>} */ iiif) => {
+		const i = Number(node.id.split('-')[1]);
+		if (!iiif) return;
+		/** @type {ResizeObserver}*/
+		let observer;
+		const createViewer = () => {
+			viewer = new OpenSeadragon.Viewer({
+				id: node.id,
+				prefixUrl: `${assets}/openseadragon-svg-icons/icons/`,
+				navImages: {
+					zoomIn: {
+						REST: 'zoomin_rest.svg',
+						GROUP: 'zoomin_grouphover.svg',
+						HOVER: 'zoomin_hover.svg',
+						DOWN: 'zoomin_pressed.svg'
+					},
+					next: {
+						REST: 'next_rest.svg',
+						GROUP: 'next_grouphover.svg',
+
+						HOVER: 'next_hover.svg',
+						DOWN: 'next_pressed.svg'
+					},
+					previous: {
+						REST: 'previous_rest.svg',
+						GROUP: 'previous_grouphover.svg',
+						HOVER: 'previous_hover.svg',
+						DOWN: 'previous_pressed.svg'
+					},
+					fullpage: {
+						REST: 'fullpage_rest.svg',
+						GROUP: 'fullpage_grouphover.svg',
+						HOVER: 'fullpage_hover.svg',
+						DOWN: 'fullpage_pressed.svg'
+					},
+					home: {
+						REST: 'home_rest.svg',
+						GROUP: 'home_grouphover.svg',
+						HOVER: 'home_hover.svg',
+						DOWN: 'home_pressed.svg'
+					},
+					zoomOut: {
+						REST: 'zoomout_rest.svg',
+						GROUP: 'zoomout_grouphover.svg',
+						HOVER: 'zoomout_hover.svg',
+						DOWN: 'zoomout_pressed.svg'
+					},
+					rotateleft: {
+						REST: 'rotateleft_rest.svg',
+						GROUP: 'rotateleft_grouphover.svg',
+						HOVER: 'rotateleft_hover.svg',
+						DOWN: 'rotateleft_pressed.svg'
+					},
+					rotateright: {
+						REST: 'rotateright_rest.svg',
+						GROUP: 'rotateright_grouphover.svg',
+						HOVER: 'rotateright_hover.svg',
+						DOWN: 'rotateright_pressed.svg'
+					},
+					flip: {
+						REST: 'flip_rest.svg',
+						GROUP: 'flip_grouphover.svg',
+						HOVER: 'flip_hover.svg',
+						DOWN: 'flip_pressed.svg'
+					}
+				},
+				sequenceMode: false
+			});
+			iiif.then((iiif) => {
+				if (iiif === 'not found') return;
+				viewer.open(iiif);
+			});
+			observer = new ResizeObserver((_entries) => {
+				setTimeout(() => {
+					viewer.viewport.goHome(false);
+				}, 50);
+			});
+			observer.observe(node);
+		};
+		if (!OpenSeadragon) {
+			import('openseadragon').then((r) => {
+				OpenSeadragon = r.default;
+				createViewer();
+			});
+		} else {
+			createViewer();
+		}
+
+		return {
+			/**
+			 * @param {Promise<Object>} iiif
+			 */
+			update(iiif) {
+				iiif.then((iiif) => viewer.open(iiif));
+			},
+			destroy() {
+				viewer.destroy();
+				if (observer) {
+					observer.disconnect();
+				}
+			}
+		};
+	};
+</script>
+
+<div id="viewer-{uuid}" class="w-full h-full" use:generateViewer={iiif}></div>
--- src/routes/textzeugen/[[sigla]]/[[thirties]]/[[verse]]/+page.svelte
+++ src/routes/textzeugen/[[sigla]]/[[thirties]]/[[verse]]/+page.svelte
@@ -1,123 +1,9 @@
 <script>
-	import { assets } from '$app/paths';
 	import TextzeugenSelector from '$lib/components/TextzeugenSelector.svelte';
+	import IIIFViewer from '$lib/components/IIIFViewer.svelte';
 
 	/** @type {import('./$types').PageData} */
 	export let data;
-
-	/**
-	 * @type {import('openseadragon')}
-	 */
-	let OpenSeadragon;
-
-	/**
-	 * @type {import('openseadragon').Viewer[]}
-	 */
-	let viewer = [];
-
-	const generateViewer = (/** @type {Element} */ node, /** @type {Promise<Object>} */ iiif) => {
-		const i = Number(node.id.split('-')[1]);
-		if (!iiif) return;
-		/** @type {ResizeObserver}*/
-		let observer;
-		const createViewer = () => {
-			viewer[i] = new OpenSeadragon.Viewer({
-				id: node.id,
-				prefixUrl: `${assets}/openseadragon-svg-icons/icons/`,
-				navImages: {
-					zoomIn: {
-						REST: 'zoomin_rest.svg',
-						GROUP: 'zoomin_grouphover.svg',
-						HOVER: 'zoomin_hover.svg',
-						DOWN: 'zoomin_pressed.svg'
-					},
-					next: {
-						REST: 'next_rest.svg',
-						GROUP: 'next_grouphover.svg',
-
-						HOVER: 'next_hover.svg',
-						DOWN: 'next_pressed.svg'
-					},
-					previous: {
-						REST: 'previous_rest.svg',
-						GROUP: 'previous_grouphover.svg',
-						HOVER: 'previous_hover.svg',
-						DOWN: 'previous_pressed.svg'
-					},
-					fullpage: {
-						REST: 'fullpage_rest.svg',
-						GROUP: 'fullpage_grouphover.svg',
-						HOVER: 'fullpage_hover.svg',
-						DOWN: 'fullpage_pressed.svg'
-					},
-					home: {
-						REST: 'home_rest.svg',
-						GROUP: 'home_grouphover.svg',
-						HOVER: 'home_hover.svg',
-						DOWN: 'home_pressed.svg'
-					},
-					zoomOut: {
-						REST: 'zoomout_rest.svg',
-						GROUP: 'zoomout_grouphover.svg',
-						HOVER: 'zoomout_hover.svg',
-						DOWN: 'zoomout_pressed.svg'
-					},
-					rotateleft: {
-						REST: 'rotateleft_rest.svg',
-						GROUP: 'rotateleft_grouphover.svg',
-						HOVER: 'rotateleft_hover.svg',
-						DOWN: 'rotateleft_pressed.svg'
-					},
-					rotateright: {
-						REST: 'rotateright_rest.svg',
-						GROUP: 'rotateright_grouphover.svg',
-						HOVER: 'rotateright_hover.svg',
-						DOWN: 'rotateright_pressed.svg'
-					},
-					flip: {
-						REST: 'flip_rest.svg',
-						GROUP: 'flip_grouphover.svg',
-						HOVER: 'flip_hover.svg',
-						DOWN: 'flip_pressed.svg'
-					}
-				},
-				sequenceMode: false
-			});
-			iiif.then((iiif) => {
-				if (iiif === 'not found') return;
-				viewer[i].open(iiif);
-			});
-			observer = new ResizeObserver((_entries) => {
-				setTimeout(() => {
-					viewer[i].viewport.goHome(false);
-				}, 50);
-			});
-			observer.observe(node);
-		};
-		if (!OpenSeadragon) {
-			import('openseadragon').then((r) => {
-				OpenSeadragon = r.default;
-				createViewer();
-			});
-		} else {
-			createViewer();
-		}
-
-		return {
-			/**
-			 * @param {Promise<Object>} iiif
-			 */
-			update(iiif) {
-				iiif.then((iiif) => viewer[i].open(iiif));
-			},
-			destroy() {
-				viewer[i].destroy();
-				if (observer) {
-					observer.disconnect();
-				}
-			}
-		};
-	};
 
 	const generateLabel = (/** @type {String[]} */ sigla) => {
 		const info = [...data.codices, ...data.fragments];
@@ -238,7 +124,7 @@
 			</section>
 			<section>
 				{#if typeof data.iiif === 'object'}
-					<div id="viewer-{i}" class="w-full h-full" use:generateViewer={data.iiif[i]}></div>
+					<IIIFViewer iiif={data.iiif[i]} />
 				{/if}
 			</section>
 		</article>
