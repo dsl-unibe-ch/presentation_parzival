fa7f8b3119aef412e5474fecf1fd32af9e537194
Sebastian Flick <sebastian.flick@wbkolleg.unibe.ch>
2024-08-22 15:37:19 +0200

    start with Einführung (unfinished)

--- src/routes/einfuehrung[[rest]]/+page.svelte
+++ src/routes/einfuehrung[[rest]]/+page.svelte
@@ -1,6 +1,199 @@
-<script>
-	/** @type {import('./$types').PageData} */
-	export let data;
-</script>
+<div class="container mx-auto">
+	<h1 class="h1 mb-4">
+		Wolfram von Eschenbach, ›Parzival‹.<br />Eine überlieferungskritische Ausgabe in digitaler Form
+	</h1>
+	<h2 class="h2">Voraussetzungen</h2>
 
-<h1>Einführung</h1>
+	<p>
+		<font size="+2">W</font>olfram von Eschenbach hat mit seinem ›Parzival‹ einen der bedeutendsten
+		Erzähltexte des europäischen Mittelalters geschaffen. Der zwischen 1200 und 1210 entstandene
+		Versroman verbindet den keltischen Artusstoff mit religiöser Gralsthematik. Als zentral
+		erscheint dabei die Frage, wie eine von Widersprüchen und Gegensätzen zerrissene Welt wieder
+		heil wird.
+		<br />
+
+		Wolfram verleiht dieser Frage in der fiktiven Gestalt seines ›Parzival‹ ein überzeitliches
+		Gepräge, und er hat damit eine intensive Rezeption hervorgerufen. Allein die heute bekannten
+		Zahlen der mittelalterlichen ›Parzival‹-Überlieferung sprechen für sich: Man kennt 16
+		(annähernd) vollständig erhaltene Handschriften, über 70 Fragmente sowie einen Druck vom Jahr
+		1477. Schätzungen der im Mittelalter kursierenden Überlieferungsträger belaufen sich auf bis zu
+		1000 Textzeugen. <br />
+
+		Seit im 18. Jahrhundert das Interesse an den volkssprachigen Dichtungen des Mittelalters
+		wiedererwacht ist, beschäftigt sich auch die moderne Literaturwissenschaft mit Wolframs
+		Gralsroman. Die dabei erarbeiteten Deutungen sind ebenso vielfältig wie kontrovers. Doch
+		orientiert sich die Exegese an einer Ausgabe, die – zu ihrer Entstehungszeit ein editorisches
+		Meisterwerk – gegenwärtigen Ansprüchen nicht mehr genügen kann. Karl Lachmanns
+		›Parzival‹-Edition von 1833 bildete für Generationen von Germanisten die maßgebliche Basis der
+		Auseinandersetzung und fand Neubearbeitungen bis ins 21. Jahrhundert. Dass sie bis heute
+		unersetzt blieb, liegt an der Fülle des handschriftlichen Materials und am Umfang des nahezu
+		25.000 Verse umfassenden ›Parzival‹-Romans. Gleichwohl ist man sich in der jüngeren Forschung
+		einig über die Notwendigkeit einer Neuausgabe, und entsprechend unbefriedigend gestaltet sich
+		die Arbeit mit einem gemeinhin als revisionsbedürftig erkannten Text. <br />
+
+		Kritik findet Lachmanns Methode der Textherstellung, die ein überlieferungsgeschichtlich nicht
+		einholbares Autororiginal zu rekonstruieren sucht. Kritik finden zahlreiche editorische
+		Entscheidungen Lachmanns, so namentlich jene, im Handschriftenapparat die
+		Überlieferungsverhältnisse nicht genau zu bezeichnen, sondern durch Gruppensiglen zu
+		verschleiern. Kritik findet schließlich die Tatsache, dass die von Lachmann abhängigen
+		Neuausgaben die Vielzahl der inzwischen bekannt gewordenen Überlieferungsträger nicht
+		hinreichend berücksichtigen. Aus diesen Defiziten ist das Desiderat einer neuen kritischen
+		Textausgabe erwachsen, die auf der Grundlage der gesamten heute bekannten Überlieferung basiert
+		(<a href="javascript:fenster('anmerkung/bumke.html')">J. Bumke</a>).
+	</p>
+
+	<h2>Methodischer Kontext</h2>
+	<p>
+		<font size="+2">D</font>ieses Desiderat findet sich auf zentrale Probleme in der philologischen
+		Theoriediskussion der germanistischen Mediävistik verwiesen. Zu nennen wären Phänomene wie das
+		Verhältnis von Aufführung und Schrift, die vielfach beobachtbare Varianz mittelalterlicher
+		Texte, Konzepte der Autorschaft und Überlieferungsgeschichte sowie Fragen einer adäquaten
+		Textherstellung und Textpräsentation. Sehr stark vereinfacht ließe sich sagen, daß die
+		Fachdebatte um zwei Standpunkte kreist, die man schlagwortartig mit den Begriffen
+		<i>New Philology</i> und <i>New Phylogeny</i> benennen könnte (bei letzterem handelt es sich um
+		einen hier in Anlehnung an die Bezeichnung
+		<i>New Philology</i> geprägten Neologismus): <br />
+
+		Die <a href="javascript:fenster('anmerkung/nphil.html')"><i>New Philology</i></a> betont die
+		handschriftliche Vielfalt und die daraus resultierende Unfestigkeit der mittelalterlichen Texte.
+		Sie tendiert dazu, die Hierarchie der einzelnen Überlieferungszeugen zugunsten eines prinzipiell
+		variablen, unfesten Status der mittelalterlichen Handschriftenkultur preiszugeben. <br />
+
+		Die <i>New Phylogeny</i> hält demgegenüber an handschriftlichen Bezügen und Gruppierungen als
+		der Basis überlieferungskritischer Untersuchungen fest. Der aus der Evolutionsbiologie stammende
+		Begriff <i>Phylogeny</i> (deutsch: <span class="doublequote">‚</span>Phylogenese<span
+			class="doublequote">‘</span
+		>) bezeichnet die stammesgeschichtliche Verwandtschaft der Arten und wird im angelsächsischen
+		Raum derzeit auf Fragen handschriftlicher Beziehungen angewandt, so etwa in der
+		<a href="javascript:fenster('anmerkung/chaucer.html')"> Chaucer-Forschung</a>.<br />
+
+		Eine kritische Neuausgabe des ›Parzival‹ muss der zu verarbeitenden Lesartenfülle und den nicht
+		unbeträchtlichen Problemen der Textherstellung vor dem methodischen Hintergrund der Polarität
+		von
+		<i>New Philology</i>
+		und <i>New Phylogeny</i> begegnen. Damit erscheint eine in der ›Parzival‹-Philologie der
+		sechziger Jahre erhobene Forderung aktueller denn je, nämlich
+		<span class="doublequote">„</span>vor der Klärung der Hss.-Verzweigung das gesamte Material, das
+		zur textkritischen Auswertung gesammelt wurde, [...] zu publizieren<span class="doublequote"
+			>“</span
+		>
+		(<a href="javascript:fenster('anmerkung/nellmann.html')">E. Nellmann</a>). <br />
+
+		Der Gedanke mag, als er 1968 vorgebracht worden ist, utopisch erschienen sein. Er lässt sich
+		jedoch heute mit Hilfe digitaler Methoden umsetzen. Eine überlieferungskritische Ausgabe in
+		digitaler Form ist die unverzichtbare Voraussetzung für jede Neuedition des ›Parzival‹.
+	</p>
+
+	<h2>Das Parzival-Projekt</h2>
+
+	<p>
+		<font size="+2">I</font>m Rahmen eines vom Schweizerischen Nationalfonds und phasenweise von der
+		Deutschen Forschungsgemeinschaft geförderten Projekts erarbeitet ein von Prof. Dr. Michael Stolz
+		geleitetes Forschungsteam an der Universität Bern eine solche digitale Ausgabe. Dabei wird eine
+		Edition nach Fassungen erstellt, die auf Volltranskriptionen aller Textzeugen beruht und in
+		Anlehnung an den von Joachim Bumke geprägten
+		<a href="javascript:fenster('anmerkung/klage.html')">Fassungsbegriff</a>
+		vier Textfassungen dokumentiert: *D (basierend u.a. auf Handschrift D), *m (basierend u.a. auf den
+		Handschriften mno), *G (basierend u.a. auf den Handschriften GI LM OQR Z), *T (basierend u.a. auf
+		den Textzeugen TUVW).<br />
+		Durch die Neuedition wird – im Sinne der <i>New Philology</i> – die Unfestigkeit des Textes in
+		ihrer überlieferungsgeschichtlichen Vielfalt sichtbar. Gleichzeitig ermöglichen digitale
+		Speicherverfahren auch – den Forderungen der <i>New Phylogeny</i> entsprechend – einen neuen
+		Blick auf handschriftliche Beziehungen. Hierbei kann auf stemmatologische Vorgehensweisen
+		zurückgegriffen werden, die der niederländische Romanist
+		<a href="javascript:fenster('anmerkung/dees.html')">Anthonij Dees</a>
+		entwickelt hat. <br />
+
+		Anstelle von historischen Genealogien handschriftlicher Stammbäume geht Dees von Verkettungen in
+		sogenannten
+		<span class="doublequote">‚</span>unrooted trees<span class="doublequote">‘</span> aus, die
+		keinen Aufhängungspunkt haben und auf die von einem Archetypus ausgehenden Verzweigungen
+		verzichten. Dieses Abstraktionsniveau macht das Aufzeigen von Beziehungen zwischen
+		handschriftlichen Varianten möglich, ohne dass daraus schon zwingend genealogische
+		Abhängigkeiten erschlossen werden. Nach demselben Prinzip verfahren phylogenetische Analysen der
+		Evolutionsbiologie, die aufgrund bestimmter Eigenschaften einzelner Arten deren Beziehungen und
+		Gruppierungen ermitteln, noch ehe daraus Schlussfolgerungen über die Stammesgeschichte selbst
+		gezogen werden. In Zusammenarbeit mit Biologen und Philologen an den Universitäten Cambridge
+		(England) und Helsinki (Finnland) konnten solche <span class="doublequote">‚</span>unrooted
+		trees<span class="doublequote">‘</span> zu verschiedenen Abschnitten des ›Parzival‹-Romans
+		erstellt werden. Vgl. dazu den
+		<a href="projektaufsaetze/Stolz_Wolfr12_SD.pdf" target="_blank">Beitrag von Michael Stolz</a> in:
+		Wolfram-Studien 23 (2014), bes. S. 459–465.
+	</p>
+
+	<h2>Editionsproben</h2>
+
+	<p>
+		<font size="+2">W</font>elche Möglichkeiten die synoptische Darstellung von Fassungen und
+		Überlieferungsträgern am Bildschirm bietet, zeigt die Abteilung
+		<a href="editionen.html" target="_blank">Editionsproben</a>
+		auf dieser Website. Dort werden auch die Verfahren der
+		<a href="javascript:erlaeuterungsFenster('erlfass.html')">Fassungsedition</a>
+		und das Modell einer
+		<details class="inline open:block">
+			<summary>Eintextedition</summary>
+			Neben der synoptischen Mehrtextedition wird auch eine Eintextedition (Lesetext) angeboten, in welcher
+			die wichtigsten editorischen Informationen gebündelt präsentiert werden. Der kritische Text ist
+			hier nach Fassung *D auf der Grundlage des St. Galler Codex 857 eingerichtet. Rechts daneben werden
+			in verkleinertem Schriftgrad die Varianten der Fassungstexte *G und *T (nach der jeweiligen Leithandschrift)
+			in normalisierter Form mit der jeweiligen Fassungssigle angegeben, wobei auch Binnenvarianten ausgewählter
+			Handschriften berücksichtigt werden. Die Varianten der Fassung *m (die Fassung *D nahe steht) sind
+			dagegen in der dritten Apparatetage untergebracht: Die erste dieser drei Apparatetagen listet die
+			Auswahl der Textzeugen auf, die bei der Angabe der Fassungsvarianten mit berücksichtigt werden.
+			Es sind dies neben den Leithandschriften D, m, G und T weitere vollständige Handschriften und gegebenenfalls
+			Fragmente, die aus textgeschichtlicher Perspektive als besonders wichtig erachtet werden. Die zweite
+			Apparatetage beinhaltet (ähnlich wie „Apparat 1“ in der synoptischen Edition) Angaben zur materiellen
+			Gestalt der ausgewählten Textzeugen; aufgeführt sind Initialen, Majuskeln sowie gegebenenfalls
+			Überschriften und Illustrationen. Die dritte Apparatetage dokumentiert Abweichungen der Leithandschrift
+			D gegenüber dem konstituierten Text *D und verzeichnet die Varianten der Fassung *m. Auf semantisch
+			wichtige Fassungsunterschiede der Fassung *m macht gegebenenfalls ein auf diese Apparatetage verweisendes
+			Pfeilsymbol in der Spalte der Fassungsvarianten (neben dem *D-Text) aufmerksam. In seltenen Fällen
+			wird das auf diesen Apparat verweisende Pfeilsymbol auch für Varianten der Fassungen *G und *T
+			verwendet, falls deren Wortlaut aus Platzgründen nicht neben dem Lesetext untergebracht werden
+			kann. Ausführlichere Erläuterungen zur Eintextedition enthalten die Beiträge: Michael Stolz: Von
+			den Fassungen zur Eintextedition. Eine neue Leseausgabe von Wolframs ›Parzival‹, in: Überlieferungsgeschichte
+			transdisziplinär. Neue Perspektivenauf ein germanistisches Forschungsparadigma, in Verbindung mit
+			Horst Brunner u. Freimut Löser hg. von Dorothea Klein, Wiesbaden 2016 (Wissensliteratur im Mittelalter
+			52), S. 353–388. Brüggen, Elke u. Michael Stolz: Fassungen, Übersetzung und Kommentar. Profile
+			einer neuen Ausgabe von Wolframs ›Parzival‹ – erscheint in: Walther von der Vogelweide. Düsseldorfer
+			Kolloquium 2018, in Verbindung mit Franz-Josef Holznagel u. Mathias Herweg hg. von Ricarda Bauschke-Hartung,
+			Berlin 2020 (Wolfram-Studien 26), S. 471–493 (im Druck)
+		</details>
+		(basierend auf<a href="hsverz.html" target="_blank">Codex 857</a> der Stiftsbibliothek St.
+		Gallen aus der Mitte des 13. Jahrhunderts, Hs. D) erläutert.<br />
+
+		Zweifellos kann in einer Digitaledition die von der <i>New Philology</i> postulierte Varianz
+		angemessener dokumentiert werden als in konventionellen Textausgaben. Die kritischen Apparate
+		traditionellen Zuschnitts teilen die Lesarten zumeist nur punktuell mit, wobei die Varianten
+		Wort für Wort wiedergegeben werden. Am Bildschirm hingegen wird die überlieferungsgeschichtliche
+		Vielfalt nachvollziehbar. Der zweite wichtige Vorteil der Digitaledition liegt jedoch gerade in
+		dem von der <i>New Phylogeny</i> geforderten Aufweis handschriftlicher Gruppierungen.
+		Computerprogramme beschleunigen die Analyseverfahren und erleichtern eine flexible Handhabe
+		handschriftlicher Zuordungen. Sie erlauben die zügige Revision philologischer Urteile über
+		Leithandschriften und stemmatologische Relationen. <br />
+
+		Somit ermöglicht die Digitaledition eine Synthese von zunächst gegensätzlich erscheinenden
+		philologischen Standpunkten. In der Kombination von Ansätzen der <i>New Philology</i> und der
+		<i>New Phylogeny</i>
+		bietet sie die unabdingbare Voraussetzung für jede kritische Neuausgabe des ›Parzival‹. Indem sie
+		stemmatologische Methoden mit überlieferungs- und mediengeschichtlichen Zugängen verbindet, stellt
+		sie darüber hinaus eine neue Editionsform eigener Prägung und eigenen Rechts dar. Die Benutzerinnen
+		und Benutzer werden dabei bis zu einem gewissen Grad an der editorischen Arbeit beteiligt und erhalten
+		Freiräume im Zugriff auf unterschiedliche Textversionen und deren handschriftliches Erscheinungsbild.
+		Die auf diese Weise bereitgestellten Überlieferungszeugnisse dürften für Literar- und Sprachhistoriker
+		gleichermaßen von Interesse sein. <br />
+
+		Die Nutzung des digitalen Mediums fügt sich somit in einen jahrhundertealten
+		Überlieferungsprozess ein - vom Zeitalter nach Gutenberg gelangen die Benutzerinnen und Benutzer
+		in die Ära vor Gutenberg. Hier zeigt sich die kulturwissenschaftliche Relevanz der digitalen
+		›Parzival‹-Ausgabe: Sie steht im Kontext aktueller Ausrichtungen der historischen
+		Wissenschaften, die sich vermehrt der überlieferungsgeschichtlichen Medialität sowie
+		diskursanalytischen und anthropologischen Fragestellungen zuwenden. Nach der an historischen
+		Großereignissen orientierten politischen Geschichte und der über die menschliche Arbeit
+		definierten Sozialgeschichte rücken Aspekte der Vermittlung, Tradierung und Speicherung
+		historischer Wissensbestände in den Vordergrund. An die Stelle des <i>homo laborans</i> tritt
+		der
+		<i>homo tradens</i>
+		der historischen Anthropologie. <br />
+	</p>
+</div>
