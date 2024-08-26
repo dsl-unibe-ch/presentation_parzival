76adadf5d60baaa381e756afffa0952d00f6830e
Sebastian Flick <sebastian.flick@wbkolleg.unibe.ch>
2024-08-26 11:21:26 +0200

    Add content Mitarbeiter

--- src/routes/mitarbeitende[[rest]]/+page.svelte
+++ src/routes/mitarbeitende[[rest]]/+page.svelte
@@ -1,4 +1,133 @@
-<script>
-	/** @type {import('./$types').PageData} */
-	export let data;
-</script>
+<div class="container mx-auto typography">
+	<h1>Mitarbeiterinnen und Mitarbeiter am Parzival-Projekt</h1>
+
+	<h2>Projektleitung</h2>
+
+	<ul>
+		<li>
+			<a href="https://parzival.unibe.ch/stolz/index1.html" target="_blank">
+				Prof. Dr. Michael Stolz
+			</a> (Universität Bern)
+		</li>
+	</ul>
+
+	<h2>Wissenschaftliche Mitarbeiterinnen und Mitarbeiter</h2>
+
+	<ul>
+		<li>
+			<a
+				href="https://www.germanistik.unibe.ch/ueber_uns/personen/pd_dr_abel_stefan/index_ger.html"
+				target="_blank">PD Dr. Stefan Abel</a
+			> (Universität Bern)
+		</li>
+		<li>Dr. Ann-Kathrin Deininger (Universität Bern)</li>
+		<li>
+			<a
+				href="https://www.germanistik.unibe.ch/ueber_uns/personen/dr_strieder_miriam/index_ger.html"
+				target="_blank">Dr. Miriam Strieder</a
+			> (Universität Bern)
+		</li>
+		<li>Dr. Agata Mazurek (Universität Bern)</li>
+	</ul>
+
+	<h2>Programmierung und Webdesign</h2>
+	<ul>
+		<li>Simone Hiltscher (Universitäten Basel und Bern, 2005 bis 2021)</li>
+		<li>
+			<a
+				href="https://www.dh.unibe.ch/ueber_uns/personen/prof_dr_hodel_tobias/index_ger.html"
+				target="_blank">Prof. Dr. Tobias Hodel</a
+			> (Digital Humanities, Universität Bern, 2024)
+		</li>
+		<li>
+			<a
+				href="https://www.dh.unibe.ch/ueber_uns/personen/daengeli_peter/index_ger.html"
+				target="_blank">Peter Dängeli</a
+			> (Digital Humanities, Universität Bern, 2024)
+		</li>
+		<li>
+			<a
+				href="https://www.dh.unibe.ch/ueber_uns/personen/flick_sebastian/index_ger.html"
+				target="_blank">Sebastian Flick</a
+			> (Digital Humanities, Universität Bern, 2024)
+		</li>
+	</ul>
+
+	<h2>Wartung und Support</h2>
+	<ul>
+		<li>Dominique Steinbach (Universität Bern)</li>
+		<li>Philipp Lammerskitten (Universität Bern)</li>
+	</ul>
+
+	<h2>Ehemalige wissenschaftliche Mitarbeiterinnen und Mitarbeiter</h2>
+
+	<ul>
+		<li>
+			<a href="https://www.glw.uni-jena.de/mitarbeiter-innen/haustein-jens" target="_blank"
+				>Prof. Dr. Jens Haustein</a
+			> (Universität Jena, Ko-Projektleitung DFG 2012–2017)
+		</li>
+		<li>
+			<a
+				href="https://www.germanistik.phil.fau.de/faecher/mediaevistik/lehrstuhl-fuer-aeltere-deutsche-literatur/univisid/49922054/"
+				target="_blank">PD Dr. Sonja Glauch</a
+			> (Universität Erlangen-Nürnberg, Ko-Projektleitung DFG 2012–2017)
+		</li>
+		<li>Letizia Carigiet M.A. (Universität Bern)</li>
+		<li>Dr. Yen-Chun Chen (Universität Bern)</li>
+		<li>
+			<a
+				href="https://www.germanistik.uni-bonn.de/institut/abteilungen/skandinavische-sprachen-und-literaturen/abteilung/personal/chlench-priber-kathrin"
+				target="_blank">Prof. Dr. Kathrin Chlench</a
+			> (Universität Bern, jetzt W3-Professorin an der Universität Bonn)
+		</li>
+		<li>
+			<a href="https://www.ku.de/suche?tx_solr%5Bq%5D=simon+falch#tx-solr-search" target="_blank"
+				>Dr. Simon Falch</a
+			> (Universität Bern, jetzt wieder Katholische Universität Eichstätt-Ingolstadt)
+		</li>
+		<li>Sonja Falk M.A. (Universität Erlangen-Nürnberg)</li>
+		<li>Dr. Richard Fasching (Universität Bern, jetzt Universität Zürich)</li>
+		<li>Marius Gehrig M.A. (Universität Bern)</li>
+		<li>Dr. Mirjam Geissbühler</li>
+		<li>
+			Christian Griesinger M.A. (Universität Bern, jetzt Lehrbeauftragter an der Universität
+			Wuppertal)
+		</li>
+		<li>Christiane Klein M.A. (Universität Bern)</li>
+		<li>Nicolas Kessler (Universität Bern)</li>
+		<li>Dr. Jörg Klenk (Berlin-Brandenburgische Akademie der Wissenschaften)</li>
+		<li>Simone Lahn M.A. (Universität Erlangen-Nürnberg)</li>
+		<li>Dr. Judith Lange (Universität Bern, jetzt Universität Duisburg-Essen)</li>
+		<li>Dr. Claudia Lingscheid (Universität Bern, jetzt in der Verwaltung der Universität Oslo)</li>
+		<li>Dr. Christoph Pretzer (Universität Bern, jetzt Marie Curie-Fellow)</li>
+		<li>Anett Rehner M.A. (Berlin-Brandenburgische Akademie der Wissenschaften)</li>
+		<li>
+			Livia Sandri M.A. (Berlin-Brandenburgische Akademie der Wissenschaften, jetzt
+			Projektmitarbeiterin Burgerbibliothek Bern)
+		</li>
+		<li>
+			<a
+				href="https://www.germanistik.unibe.ch/ueber_uns/personen/pd_dr_schoeller_robert/index_ger.html"
+				target="_blank">PD Dr. Robert Schöller</a
+			> (Universität Bern)
+		</li>
+		<li>
+			<a
+				href="https://www.uni-due.de/zim/services/suchdienste/mitarbeiter.php?id=58239&amp;vcard="
+				target="_blank">Prof. Dr. Martin Schubert</a
+			> (Berlin-Brandenburgische Akademie der Wissenschaften, jetzt Professor für Germanistische Mediävistik,
+			Universität Duisburg-Essen)
+		</li>
+		<li>
+			<a href="http://www.uni-stuttgart.de/dh/Abteilung/Mitarbeiter/Viehhauser.html" target="_blank"
+				>Prof. Dr. Gabriel Viehhauser</a
+			> (Universität Bern, jetzt W3-Professor für Digital Humanities an der Universität Stuttgart)
+		</li>
+		<li>
+			Dr. Lydia Wegener (Universität Bern, jetzt Wissenschaftliche Mitarbeiterin, Deutsche Texte
+			Mittelalter, Berlin-Brandenburgische Akademie der Wissenschaften, Lehrbeauftragte
+			Humboldt-Univerität zu Berlin)
+		</li>
+	</ul>
+</div>
