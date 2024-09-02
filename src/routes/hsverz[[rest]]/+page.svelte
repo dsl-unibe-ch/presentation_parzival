66d2ae79c6f6632f81c2463783cea85864969ee0
Sebastian Flick <sebastian.flick@wbkolleg.unibe.ch>
2024-09-02 15:17:13 +0200

    allow array for codices

--- src/routes/hsverz[[rest]]/+page.svelte
+++ src/routes/hsverz[[rest]]/+page.svelte
@@ -6,24 +6,30 @@
 
 <div class="container mx-auto typography">
 	<h1 class="h1">Parzival-Handschriften und Fragmente zu den Editionsproben</h1>
-	{#each sigla.codices as codex}
-		<h2>Hs. {codex.sigil}</h2>
-		<p>{codex.loc}, {codex.cod}</p>
-		{#if codex.info}
-			<p>{@html codex.info}</p>
-		{/if}
-	{/each}
-	{#each sigla.fragments as fragment}
-		<h2>{@html fragment.sigil}</h2>
-		{#if typeof fragment.loc === 'string'}
-			<p>{fragment.loc}, {fragment.cod}</p>
+	{#each sigla.codices as { sigil, loc, cod, info }}
+		<h2>Hs. {sigil}</h2>
+		{#if typeof loc === 'string'}
+			<p>{loc}, {cod}</p>
 		{:else}
-			{#each fragment.loc as loc, i}
-				<p>{loc}, {fragment.cod[i]}</p>
+			{#each loc as location, i}
+				<p>{location}, {cod[i]}</p>
 			{/each}
 		{/if}
-		{#if fragment.info}
-			<p>{@html fragment.info}</p>
+		{#if info}
+			<p>{@html info}</p>
+		{/if}
+	{/each}
+	{#each sigla.fragments as { sigil, loc, cod, info }}
+		<h2>{@html sigil}</h2>
+		{#if typeof loc === 'string'}
+			<p>{loc}, {cod}</p>
+		{:else}
+			{#each loc as location, i}
+				<p>{location}, {cod[i]}</p>
+			{/each}
+		{/if}
+		{#if info}
+			<p>{@html info}</p>
 		{/if}
 	{/each}
 </div>
