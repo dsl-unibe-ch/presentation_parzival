4110998322a41ba6e39882c3bef1db43918f289f
Sebi <sebastian@flick.solutions>
2024-09-05 13:35:00 +0200

    syntactic sugar: reduce amount of lines

--- src/routes/hsverz[[rest]]/+page.svelte
+++ src/routes/hsverz[[rest]]/+page.svelte
@@ -6,14 +6,7 @@
 
 <div class="container mx-auto typography">
 	<h1 class="h1">Parzival-Handschriften und Fragmente zu den Editionsproben</h1>
-	{#each sigla.codices as { "info-h1": sigil, "info-h2": loc, info }}
-		<h2>{sigil}</h2>
-		<div>{@html loc}</div>
-		{#if info}
-			<p>{@html info}</p>
-		{/if}
-	{/each}
-	{#each sigla.fragments as { "info-h1": sigil, "info-h2": loc, info }}
+	{#each [...sigla.codices, ...sigla.fragments] as { "info-h1": sigil, "info-h2": loc, info }}
 		<h2>{sigil}</h2>
 		<div>{@html loc}</div>
 		{#if info}
