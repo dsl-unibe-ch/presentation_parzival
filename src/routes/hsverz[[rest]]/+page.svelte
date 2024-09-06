0111e1b144eeefacffad1ae9ece3fd6efdc84d10
Sebastian Flick <sebastian.flick@wbkolleg.unibe.ch>
2024-09-06 12:20:22 +0200

    styling

--- src/routes/hsverz[[rest]]/+page.svelte
+++ src/routes/hsverz[[rest]]/+page.svelte
@@ -7,8 +7,10 @@
 <div class="container mx-auto typography">
 	<h1 class="h1">Parzival-Handschriften und Fragmente zu den Editionsproben</h1>
 	{#each [...sigla.codices, ...sigla.fragments] as { "info-h1": sigil, "info-h2": loc, info }}
-		<h2>{sigil}</h2>
-		<div>{@html loc}</div>
+		<h2>{@html sigil}</h2>
+		<div class="[&_ul]:!list-none [&_ul]:!pl-0 [&_li]:font-bold">
+			{@html loc}
+		</div>
 		{#if info}
 			<p>{@html info}</p>
 		{/if}
