32c7ee66018a550226a9e490345f4a79a201a7cc
Sebastian Flick <sebastian.flick@wbkolleg.unibe.ch>
2024-09-27 19:21:43 +0200

    add docker buildstep

--- src/lib/constants.js
+++ src/lib/constants.js
@@ -1,5 +1,5 @@
 export const api = `https://dhbern.github.io/parzival-static-api/api`;
 
-export const teipb = `https://tei-ub.dh.unibe.ch/exist/apps/parzival/api`;
+export const teipb = `http://localhost:8081/exist/apps/parzival/api`;
 
 export const iiif = `https://iiif.ub.unibe.ch/image/v3/Parzival`;
